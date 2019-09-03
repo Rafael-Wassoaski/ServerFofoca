5392dcab6d3ab480190608e43a520a63f0ca9360
Rafael-Wassoaski <meireles200@hotmail.com>
2019-09-03 16:29:34 -0300

    mais 2

--- src/fofoca2/Serrver.java
+++ src/fofoca2/Serrver.java
@@ -1,7 +1,15 @@
 package fofoca2;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 import java.util.concurrent.Executors;
 
 
@@ -9,10 +17,21 @@ public class Serrver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Socket cliente1 = new Socket();
+		ThreadServer threadInicial = new ThreadServer(cliente1);
+		
+		try {
+			threadInicial.enviar("RAFAEL; Meu nome aqui");
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		
 		
-		
-		
+		//server roda aqui
 		 try (var listener = new ServerSocket(6668)) {
 	            System.out.println("Servidor rodando");
 	            var pool = Executors.newFixedThreadPool(20);
--- src/fofoca2/ThreadServer.java
+++ src/fofoca2/ThreadServer.java
@@ -1,10 +1,12 @@
 package fofoca2;
 
+import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -18,13 +20,14 @@ public class ThreadServer implements Runnable{
 	public ThreadServer(Socket cliente) {
 		
 	this.cliente = cliente;
-		
+
 		
 	}
 	
 	private void pegarIps() {
 		try {
 			String ipAddress = null;
+			Socket socket;
 			  Enumeration<NetworkInterface> net = null;
 			  net = NetworkInterface.getNetworkInterfaces();
 			  
@@ -51,8 +54,9 @@ public class ThreadServer implements Runnable{
 
 			  InetAddress inet;
 			  for(int i =Integer.parseInt(teuIp[3]) - 1; i > 0; i--) {
+				  socket = new Socket(String.format("%s.%s.%s.%d", teuIp[0], teuIp[1], teuIp[2], i), 6668);
 				  inet = InetAddress.getByName(String.format("%s.%s.%s.%d", teuIp[0], teuIp[1], teuIp[2], i));
-				  if(inet.isReachable(500)) {
+				  if(inet.isReachable(500) && socket.isConnected()) {
 					  ip.add(String.format("%s.%s.%s.%d", teuIp[0], teuIp[1], teuIp[2], i));
 					  System.out.println("Seu vizinho de baixo: "+String.format("%s.%s.%s.%d", teuIp[0], teuIp[1], teuIp[2], i));
 					  break;
@@ -61,8 +65,9 @@ public class ThreadServer implements Runnable{
 			  
 			  
 			  for(int i =Integer.parseInt(teuIp[3]) + 1; i < 256; i++) {
+				  socket = new Socket(String.format("%s.%s.%s.%d", teuIp[0], teuIp[1], teuIp[2], i), 6668);
 				  inet = InetAddress.getByName(String.format("%s.%s.%s.%d", teuIp[0], teuIp[1], teuIp[2], i));
-				  if(inet.isReachable(500)) {
+				  if(inet.isReachable(500) && socket.isConnected()) {
 					  ip.add(String.format("%s.%s.%s.%d", teuIp[0], teuIp[1], teuIp[2], i));
 					  System.out.println("Seu vizinho de cima: "+String.format("%s.%s.%s.%d", teuIp[0], teuIp[1], teuIp[2], i));
 					  break;
@@ -71,6 +76,37 @@ public class ThreadServer implements Runnable{
 			
 		}catch (Exception e) {
 			System.out.println(e.getMessage());
+		}
+	}
+	
+	public void enviar(String fofoca) throws UnknownHostException, IOException {
+		
+		
+		pegarIps();
+		
+		if(ip.isEmpty()) {
+			System.out.println("Sem vizinhos, não posso mandar a fofoca");
+		}else {
+			
+			
+	
+		
+		
+		String[] fofocaFrag = fofoca.split(";");
+		OutputStream output;
+		PrintWriter pr;
+		
+		
+		Socket envio;
+		for(String ipEnvio : ip) {
+			
+			if(cliente.getInetAddress() != ip) {
+				envio = new Socket(ipEnvio, 6668);
+				output = envio.getOutputStream();
+				pr = new PrintWriter(output, true);
+				pr.println(fofoca);
+			}
+		}
 		}
 	}
 
@@ -88,29 +124,9 @@ public class ThreadServer implements Runnable{
 		
 		
 		
-		pegarIps();
 		
-		if(ip.isEmpty()) {
-			System.out.println("Sem vizinhos, não posso mandar a fofoca");
-		}else {
-			String[] fofocaFrag = fofoca.split(";");
-			OutputStream output;
-			PrintWriter pr;
-			
-			
-			Socket envio;
-			
-			for(String ipEnvio : ip) {
-				
-				if(cliente.getInetAddress() != ip) {
-					envio = new Socket(ipEnvio, 6668);
-					output = envio.getOutputStream();
-					pr = new PrintWriter(output, true);
-					pr.println("RAFAEL;"+fofocaFrag[1]);
-				}
-			}
-
-		}
+		
+		
 		
 		
 		}catch (Exception e) {
